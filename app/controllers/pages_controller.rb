af3d568ece9559c06b59daf5f84ec82f5188dee8
Marcela Fernandez <marcelaf@andrew.cmu.edu>
2011-09-22 22:55:39 -0400

    varios

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
   def home
+    @title = "Fernandez Pastry"
   end
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe PagesController do
-
+  render_views
+  describe "GET 'home'" do
+    it "should be successful" do
+      get 'home'
+      response.should be_success
+    end
+  end
+  
+  it "should have the right title" do
+      get 'home'
+      response.should have_selector("title",
+                        :content => "Fernandez Pastry")
+  end
 end
